1f06c9baa8c728715dc36666c758b70c21129e6c
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-08-15 16:50:44 +0200

    Added stuff to getSym

--- Declarations.cs
+++ Declarations.cs
@@ -49,27 +49,31 @@
       noSym        =  0,
       EOFSym       =  1,
       identSym     =  2,
-      numSym       =  3,
-      lparenSym    =  4,
-      rparenSym    =  5,
-      OFSym        =  6,
-      TOSym        =  7,
-      typeSym      =  8,
-      varSym       =  9,
-      equalSym     =  10,
+      numSym       =  3, 
+      lparenSym    =  4, 
+      rparenSym    =  5, 
+      OFSym        =  6, //"OF"
+      TOSym        =  7, //"TO"
+      typeSym      =  8, //"TYPE"
+      varSym       =  9, //"VAR"
+      equalSym     =  10, 
       lsquareSym   =  11,
       rsquareSym   =  12,
-      arraySym     =  13, //
-      recordSym    =  14, //
-      singleDotSym =  15,
-      doubleDotSym =  16,
-      commaSym     =  17,
-      pointerSym   =  18,
-      semiColonSym =  19,
-      endSym       =  20;
+      arraySym     =  13, //"ARRAY"
+      recordSym    =  14, //"RECORD"
+      setSym       =  15, //"SET"
+      singleDotSym =  16, //"."
+      doubleDotSym =  17, //".."
+      commaSym     =  18,
+      pointerSym   =  19, //"POINTER"
+      semiColonSym =  20, 
+      colonSym     =  21,
+      lcurveSym    =  22,
+      rcurveSym    =  23,
+      endSym       =  24;  //"END"
 
 
-
+    // +++++++++++++++++++++++  List of Syms +++++++++++++++++++++++++
 
     // +++++++++++++++++++++++++++++ Character Handler ++++++++++++++++++++++++++
 
@@ -101,16 +105,110 @@
 
     static void GetSym() {
     // Scans for next sym from input
-      while (ch > EOF && ch <= ' ') GetChar();
       StringBuilder symLex = new StringBuilder();
       int symKind = noSym;
+      while (ch > EOF && ch <= ' ') GetChar();
+      int symKind = noSym;
 
-      // over to you! 
-      {
-        
-      }
+        if (Char.IsLetter(ch))
+        {
+            do
+            {
+                symLex.Append(ch); GetChar();
+            } while (Char.IsLetterOrDigit(ch) || ch == '.'); // need to change this.
+            //checks if special thingymobob
+            switch (symLex)
+            {
+                case "OF":
+                    symKind = OFSym;  GetChar();
+                    break;
+                case "TO":
+                    symKind = TOSym;  GetChar();
+                    break;
+                case "TYPE":
+                    symKind = typeSym; GetChar();
+                    break;
+                case "VAR":
+                    symKind = varSym; GetChar();
+                    break;
+                case "ARRAY":
+                    symKind = arraySym; GetChar();
+                    break;
+                case "RECORD":
+                    symKind = recordSym; GetChar();
+                    break;
+                case "SET":
+                    symKind = setSym; GetChar();
+                    break;
+                case ".":
+                    symKind = singleDotSym; GetChar();
+                    break;
+                case "..":
+                    symKind = doubleDotSym; GetChar();
+                    break;
+                case "POINTER":
+                    symKind = pointerSym; GetChar();
+                    break;
+                case "END":
+                    symKind = endSym; GetChar();
+                    break;
+                default: symKind = identSym;
+            }
+            symKind = identSym;
+        }
+        else if (Char.IsDigit(ch))
+        {
+            do
+            {
+                symLex.Append(ch); GetChar();
+            } while (Char.IsDigit(ch));
+            symKind = numSym;
+        }
+        else
+        {
+            symLex.Append(ch);
+            switch (ch)
+            {
+                case EOF:
+                    symLex = new StringBuilder("EOF"); // special case
+                    break; // no need to GetChar
+                case '=':
+                    symKind = equalSym; GetChar();
+                    break;
+                case '(':
+                    symKind = lparenSym; GetChar();
+                    break;
+                case ')':
+                    symKind = rparenSym; GetChar();
+                    break;
+                case '[':
+                    symKind = lsquareSym; GetChar();
+                    break;
+                case ']':
+                    symKind = rsquareSym; GetChar();
+                    break;
+                case '{':
+                    symKind = lcurveSym; GetChar();
+                    break;
+                case '}':
+                    symKind = rcurveSym; GetChar();
+                    break;
+                case ':':
+                    symKind = colonSym; GetChar();
+                    break;
+                case ';':
+                    symKind = semiColonSym; GetChar();
+                    break;
+                case ',':
+                    symKind = commaSym; GetChar();
+                    break;
+                default:
+                    symKind = noSym; GetChar();
+                    break;
+            }
+        }
 
-      sym = new Token(symKind, symLex.ToString());
+        sym = new Token(symKind, symLex.ToString());
     } // GetSym
 
   /*  ++++ Commented out for the moment
